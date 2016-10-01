ae0109e37477718d0623a19a5fbda35446ddf955
Jonny Page <abplagen@gmail.com>
2016-10-01 15:15:48 +0100

    Various enhancements
    - Removed google analytics and twitter nonsense.
    - Squeezed the top of the screen up a bit so that grid doesn't need to
    be scrolled; 
    - Reduced animation times to display game over message.

--- js/animframe_polyfill.js
+++ js/animframe_polyfill.js
@@ -10,7 +10,8 @@
   if (!window.requestAnimationFrame) {
     window.requestAnimationFrame = function (callback) {
       var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+//      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var timeToCall = 0;
       var id = window.setTimeout(function () {
         callback(currTime + timeToCall);
       },
--- js/html_actuator.js
+++ js/html_actuator.js
@@ -40,9 +40,9 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 
 // Continues the game (both restart and keep playing)
 HTMLActuator.prototype.continueGame = function () {
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "restart");
-  }
+//  if (typeof ga !== "undefined") {
+//    ga("send", "event", "game", "restart");
+//  }
 
   this.clearMessage();
 };
@@ -135,16 +135,16 @@ HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : "Game over!";
 
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "end", type, this.score);
-  }
+//  if (typeof ga !== "undefined") {
+//    ga("send", "event", "game", "end", type, this.score);
+//  }
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
 
   this.clearContainer(this.sharingContainer);
   this.sharingContainer.appendChild(this.scoreTweetButton());
-  twttr.widgets.load();
+//  twttr.widgets.load();
 };
 
 HTMLActuator.prototype.clearMessage = function () {
